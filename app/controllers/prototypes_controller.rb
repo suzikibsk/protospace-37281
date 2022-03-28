5e07d4b6d589232a72b6813a37340622ad017d37
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-03-28 12:02:54 +0900

    update prototypes_controller.rb2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confimation, only: [:edit, :update, :destroy]
   
@@ -56,9 +56,6 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to action: :index
-  end
 
   def contributor_confimation
     redirect_to root_path unless current_user == @prototype.user
